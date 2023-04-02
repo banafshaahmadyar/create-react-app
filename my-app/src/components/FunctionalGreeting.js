7ae1cd0889e94f7d65639acba2d01db36ff0d915
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-04-02 08:54:42 +0000

    Used funtional components

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function FunctionalGreeting(){
+//     return <h2>Hi from functional Greeting </h2>
+// }
+
+
+// arrow function
+const FunctionalGreeting =()=> <h1>Hello from FunctionalGreeting(:</h1>
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,5 @@
+import React from "react";
+// arrow function
+const FunctionalGreetingWithProps =()=> <h1>Hello from FunctionalGreetingWithProps(:</h1>
+
+export default FunctionalGreetingWithProps;
